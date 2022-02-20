29f90da475e4804eb13d4ffff65140d798f45b34
Kubatov Susa <dom.ru0@yandex.ru>
2022-02-20 13:10:37 +0300

    feat: Add redis

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import { ApolloServer } from 'apollo-server';
-import responseCachePlugin from 'apollo-server-plugin-response-cache'
+import responseCachePlugin from 'apollo-server-plugin-response-cache';
+import * as Redis from 'ioredis';
+
 import { resolvers } from './schema';
 import { createContext } from './context';
 import { makeExecutableSchema } from 'graphql-tools';
@@ -9,14 +11,20 @@ import { feed } from './typeDefs/feed';
 import { source } from './typeDefs/source';
 import { post } from './typeDefs/post';
 import { mutation } from './typeDefs/mutation';
+import { BaseRedisCache } from 'apollo-server-cache-redis';
 
 
 const schema = makeExecutableSchema({
   typeDefs: [query, user, feed, source, post, mutation],
-  resolvers
+  resolvers,
 });
 
-new ApolloServer({ schema, context: createContext, plugins: [responseCachePlugin()] }).listen(
+const client = new Redis({ host: 'localhost', port: 6379 });
+const cache = new BaseRedisCache({ client });
+const app = new ApolloServer({ schema, context: createContext, plugins: [responseCachePlugin()], cache });
+
+
+app.listen(
   { port: 4000 },
   () => console.log(`🚀 Server ready at: http://localhost:4000 ⭐️⭐️⭐️⭐️`),
 ).catch((e) => {
